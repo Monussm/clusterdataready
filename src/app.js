020a9465ceb6bd5bc96cc42e43a4cca6065aa8ba
Monu gupta <monug1513@gmail.com>
2023-04-28 08:54:26 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,55 @@
+const express=require('express');
+const port=process.env.port || 3000;
+const app=express();
+const path=require('path');
+const hbs=require('hbs');
+const mypublic=path.join(__dirname,"../public");
+const mypartials=path.join(__dirname,"../partials");
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(mypublic));
+app.set("view engine","hbs");
+hbs.registerPartials(mypartials)
+// getting-started.js
+const mongoose = require('mongoose');
+main().catch(err => console.log(err));
+async function main() {
+await mongoose.connect('mongodb+srv://monug1513:monu123@work.eqglgeu.mongodb.net/test');
+console.log("data connected");
+
+}
+
+
+
+app.get("/",(req,res)=>{
+
+res.render("index")
+
+})
+
+app.get("/men",(req,res)=>{
+res.render("men")
+})
+
+
+
+app.get("/women",(req,res)=>{
+res.render("women")
+})
+
+app.get("/kid",(req,res)=>{
+    res.render("kid")
+})
+
+
+
+
+
+
+
+app.listen(port,(req,res)=>{
+
+console.log("Running on port 3000")
+
+
+
+})
